dd01584ae2d8db31274f3f48fa5f890eabd1d62d
Moshu <sellfor59@gmail.com>
2026-04-24 18:58:38 +0500

    refactor: complete removal of Tailwind CSS and transition to agency-grade vanilla CSS architecture

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,7 @@
-import React from 'react';
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return clsx(inputs);
 }
 
 export function dateKey(d: Date) {
